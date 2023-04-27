4fb727f1f1b3872941eccb0038701ec77317a118
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-04-27 23:10:19 +0900

    JsonResponse and models

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=19, decimal_places=1)
+
+    def __str__(self):
+        return  self.name
+    
--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .models import Employee
 # Create your views here.
 
 def index(request):
     return render(request, 'my_app/index.html')
 
 def employeeView(request):
-    employee = {
-        'id':123,
-        'name': "Mustang",
-        "salary": 1900000,
-    }
-    return JsonResponse(employee)
+    data = Employee.objects.all()
+    response = {'employee':list(data.values('id','name','salary'))}
+    return JsonResponse(response)
